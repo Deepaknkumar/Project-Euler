baa3a2a1df2aee5a9e3ffafd944eb5985b5f5712
Deepak Kumar <Deepak.n.sonukumar@gmail.com>
2017-02-19 02:37:45 -0800

    Problem 1
    Problem 1 Added

---
+++ Project-Euler/Project-Euler/Problem1.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Author : Deepak Kumar (Deepak.n.sonukumar@gmail.com)
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Euler
+{
+    class Problem1
+    {
+        // Problem Statement:
+        // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+        // Find the sum of all the multiples of 3 or 5 below 1000.
+
+        public double result { get; set; }
+        public double initValue { get; set; }
+        public double finalValue { get; set;}
+
+        public Problem1()
+        {
+            this.initValue = 0;
+            this.finalValue = 1000;
+        }
+
+        public Problem1(double init, double finalVal)
+        {
+            this.initValue = init;
+            this.finalValue = finalVal;
+        }
+
+        public double computeSumOf3Or5Multiples()
+        {
+            result = computeSum();
+            return result;
+        }
+
+        private double computeSum() {
+            double sumVal = 0;
+            for(int i=0; i < this.finalValue; i++)
+            {
+                if ((i%3 == 0) || (i%5 == 0))
+                {
+                    sumVal += i;
+                }
+            }
+            return sumVal;
+        }
+    }
+}
---
+++ Project-Euler/Project-Euler/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Euler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Problem2 p2 = new Problem2();
+            p2.computeSum();
+            Console.WriteLine("The Result is {0}\n", p2.result);
+
+            //Problem1 p1 = new Problem1();
+            //p1.computeSumOf3Or5Multiples();
+            //Console.WriteLine("The Result is {0}\n", p1.result);
+
+        }
+    }
+}
