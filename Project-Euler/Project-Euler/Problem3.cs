ae6c4301aa96f1c3287a1f3f8601f06c9abc1c07
Deepak Kumar <Deepak.n.sonukumar@gmail.com>
2017-02-19 02:39:15 -0800

    Problem 3
    Problem 3 with Question Added

---
+++ Project-Euler/Project-Euler/Problem3.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Author : Deepak Kumar (Deepak.n.sonukumar@gmail.com)
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Euler
+{
+    class Problem3
+    {
+        // Problem 3: Largest Prime Factor
+        // The prime factors of 13195 are 5, 7, 13 and 29.
+        // What is the largest prime factor of the number 600851475143 ?
+
+
+    }
+}
