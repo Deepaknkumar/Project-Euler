2755a7554f8481cb576001e1345d43c04668d44d
Deepak Kumar <Deepak.n.sonukumar@gmail.com>
2017-02-22 23:50:03 -0800

    Problem 3
    Problem 3 in Progress

--- Project-Euler/Project-Euler/Problem3.cs
+++ Project-Euler/Project-Euler/Problem3.cs
@@ -16,6 +16,34 @@ namespace Project_Euler
         // The prime factors of 13195 are 5, 7, 13 and 29.
         // What is the largest prime factor of the number 600851475143 ?
 
+        public double result { get; set; }
+        public double num { get; set; }
+        
+        public Problem3(double num)
+        {
+            this.num = num;
+        }
+
+        public List<Double> FindFactors() {
+            List<Double> factors = new List<double>();
+            for(int i = 1; i < Math.Sqrt(this.num)+1; i+=2)
+            {
+                if((num/i) == 0)
+                {
+                    factors.Add(i);
+                }
+            }
+            return factors;
+        }
+
+        public double FindLargestPrimeFactor(List<Double> factors) {
+            double largestPrimeFactor = 1;
+            
+            foreach(double fac in factors)
+            {
+                if(fac)
+            }
+        }
 
     }
 }
